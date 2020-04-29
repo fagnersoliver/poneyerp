44225895e58c205a9120450d525bb8905ba46c61
Fagner Oliveira <fagner.soliver@gmail.com>
2020-04-29 18:30:36 -0300

    Projeto foi criado

---
+++ src/main/java/br/com/poneyerp/PoneyerpApplication.java
@@ -0,0 +1,13 @@
+package br.com.poneyerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoneyerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoneyerpApplication.class, args);
+	}
+
+}

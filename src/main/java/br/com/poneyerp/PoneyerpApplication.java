de963a49a2753179e3594946aa3b597c36a2b1a6
Fagner Oliveira <fagner.soliver@gmail.com>
2020-05-06 22:45:32 -0300

    Criando operacao de instẽncia

--- src/main/java/br/com/poneyerp/PoneyerpApplication.java
+++ src/main/java/br/com/poneyerp/PoneyerpApplication.java
@@ -1,13 +1,34 @@
 package br.com.poneyerp;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.poneyerp.domain.Categoria;
+import br.com.poneyerp.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class PoneyerpApplication {
+public class PoneyerpApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PoneyerpApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }

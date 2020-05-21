88f43b0ad28c6059c53548e9df58fdc881a7fcd0
Fagner Oliveira <fagner.soliver@gmail.com>
2020-05-21 16:23:02 -0300

    Incluido estado/cidade

--- src/main/java/br/com/poneyerp/PoneyerpApplication.java
+++ src/main/java/br/com/poneyerp/PoneyerpApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.poneyerp.domain.Categoria;
+import br.com.poneyerp.domain.Cidade;
+import br.com.poneyerp.domain.Estado;
 import br.com.poneyerp.domain.Produto;
 import br.com.poneyerp.repositories.CategoriaRepository;
+import br.com.poneyerp.repositories.CidadeRepository;
+import br.com.poneyerp.repositories.EstadoRepository;
 import br.com.poneyerp.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class PoneyerpApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PoneyerpApplication.class, args);
@@ -48,10 +58,23 @@ public class PoneyerpApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		// Salvando as categorias
+		// Salvando as categorias/produtos
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "São Paulo");
+		Estado est2 = new Estado(null, "Minas Gerais");
+
+		Cidade c1 = new Cidade(null, "Cotia", est1);
+		Cidade c2 = new Cidade(null, "Caucaia", est1);
+		Cidade c3 = new Cidade(null, "Uberlândia", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1, c2));
+		est2.getCidades().addAll(Arrays.asList(c3));
+
+		// Salvando as cidade/estado
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 	}
 
---
+++ src/main/java/br/com/poneyerp/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package br.com.poneyerp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.poneyerp.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
